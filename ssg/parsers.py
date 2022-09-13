7712b24b6b6d792ab6f0fcddaef1dc2f8006c35d
AlanMcClean <87909322+AlanMcClean@users.noreply.github.com>
2022-09-13 10:45:32 +0100

    made parser pass 2

--- ssg/parsers.py
+++ ssg/parsers.py
@@ -1,3 +1,4 @@
+import shutil
 from typing import List
 from pathlib import Path
 
@@ -11,11 +12,21 @@ class Parser:
     def parse(self, path: Path, source: Path, dest: Path):
         raise NotImplementedError
 
-    def read(self, path: Path):
+    def read(self, path):
         with open(path, "r") as file:
             return file.read()
 
-    def write(self, path: Path, dest: Path, content, ext=".html"):
-        full_path = self.dest / path.with_suffix(ext)
+    def write(self, path, dest, content, ext=".html"):
+        full_path = dest / path.with_suffix(ext).name
         with open(full_path, "w") as file:
-            return file.write(content)            
+            file.write(content)            
+
+    def copy(path, source, dest):
+        shutil.copy2(path, dest / path.relative_to(source))
+
+
+class ResourceParser(Parser):
+    extensions: List[str] = [".jpg", ".png", ".gif", ".css", ".html"]
+
+    def parse(self, path: Path, source: Path, dest: Path):
+        self.copy(path,source,dest)
