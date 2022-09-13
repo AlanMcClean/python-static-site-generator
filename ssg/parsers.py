6b3fa6c88aa23768b9d7af1089d0f55259c9cd94
AlanMcClean <87909322+AlanMcClean@users.noreply.github.com>
2022-09-13 09:56:33 +0100

    made parser pass 1

---
+++ ssg/parsers.py
@@ -0,0 +1,21 @@
+from typing import List
+from pathlib import Path
+
+class Parser:
+    extensions: List[str] = []
+
+    def valid_extension(self, extension):
+        return extension in self.extensions
+
+    
+    def parse(self, path: Path, source: Path, dest: Path):
+        raise NotImplementedError
+
+    def read(self, path: Path):
+        with open(path, "r") as file:
+            return file.read()
+
+    def write(self, path: Path, dest: Path, content, ext=".html"):
+        full_path = self.dest / path.with_suffix(ext)
+        with open(full_path, "w") as file:
+            return file.write(content)            
