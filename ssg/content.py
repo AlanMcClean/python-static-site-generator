abc9aee9abb6adf86718ec248610fcef6ebd6ecc
AlanMcClean <87909322+AlanMcClean@users.noreply.github.com>
2022-09-13 11:28:08 +0100

    made content pass 1

---
+++ ssg/content.py
@@ -0,0 +1,14 @@
+from importlib.metadata import metadata
+import re
+from yaml import load, FullLoader
+from collections.abc import Mapping
+
+class Content(Mapping):
+    __delimeter = "^(?:-|\+){3}\s*$"
+    __regex = re.compile(__delimeter, re.MULTILINE)
+
+    def load(cls, string):
+        _, fm, content = Content.__regex.split(string, 2) 
+        load(fm, Loader=FullLoader)
+        return cls(metadata, content)
+
